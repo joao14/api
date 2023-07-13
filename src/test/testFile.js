1f2d5643b14e8c9bf2b75dc4742f0e765740f731
Alexander Merino <barcealex1014@hotmail.com>
2023-07-13 00:03:49 -0500

    feat: install mocha and chai with test

---
+++ src/test/testFile.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+//const file = require('../api/components/file')
+
+//Resultados
+
+describe('File suite', function () {
+    it('Download', function () {
+        assert.equal("2", "2")
+    })
+})
